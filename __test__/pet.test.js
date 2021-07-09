ba5e3288811ba20cd0e60d20ff8d671f1a6ad9e1
Mahmoud Jeilani <jeilani@gmail.com>
2021-07-09 21:23:05 +0100

    protptype function added

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -3,8 +3,18 @@ const pet = require('../src/pet')
 
 describe('constructor', () => {
     it('returns an object', () => {
-      expect(new pet('Fido')).toBeInstanceOf(Object);
+      expect(new pet('cat')).toBeInstanceOf(Object);
+        
     });
   });
+
+  describe('growUp',()=>{
+     it('ages the pet by one year',() => {
+       const cat = new pet('muudi')
+
+      cat.growUp()
+       expect(cat.age).toEqual(1)
+     });
+  })
 
 
